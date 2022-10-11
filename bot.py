e4f6d074ad0ff4f107959f8bcbb6d2e50455584e
bemxio <bemxiov@protonmail.com>
2022-10-11 20:10:32 +0200

    implement the stuff in the markov module onto the bot

--- bot.py
+++ bot.py
@@ -1,55 +1,38 @@
-import markovify
+from modules.markov import ConversationalChain
 import fbchat
-import spacy
-
-from typing import List
 import json
 
-class Robotczyk(fbchat.Client):
-    def __init__(self, dataset: List[str], mentions: List[str], *args, **kwargs):    
-        self.nlp = spacy.load("pl_core_news_sm")
-
-        self.mentions = mentions
-        self.dataset = dataset
+class RobotczykClient(fbchat.Client):
+    def __init__(self, chain: ConversationalChain, *args, **kwargs):
+        self.chain = chain
+        self.mentions = ["@kornelia krawiec", "@kornelia"] # TODO: fetch that information on the client's initialization
 
         super().__init__(*args, **kwargs)
     
-    # helper functions (mostly for mentions)
-    def is_mentioned(self, message_object: fbchat.Message) -> bool:
-        if message_object.replied_to is not None and message_object.replied_to.author == self.uid:
+    # helper functions
+    def is_mentioned(self, message: fbchat.Message) -> bool:
+        if message.replied_to is not None and message.replied_to.author == self.uid:
             return True
-        
-        content = message_object.text.lower()
 
-        for mention in self.mentions:
+        content = message.text.lower()
+
+        for mention in self.mentions: 
             if mention in content:
                 return True
     
         return False
 
-    def cleanify(self, content: str) -> str:
-        clean = content.lower()
-        
+    def clean_message(self, content: str) -> str:
+        clean_content = content.lower()
+
         for mention in self.mentions:
-            if not mention in clean:
+            if not mention in clean_content:
                 continue
             
-            clean = clean.replace(mention, "")
+            clean_content = clean_content.replace(mention, "")
         
-        return clean
+        return clean_content
     
-    def keyword(self, content: str) -> str:
-        if not content:
-            return ""
-        
-        doc = self.nlp(content)
-        subjects = [token.text for token in doc if token.dep_ in ("nsubj", "nsubj:pass")]
-
-        if subjects:
-            return subjects[0]
-        else:
-            return next((token.text for token in doc if token.dep_ == "ROOT"), "")
-        
     # event listeners
     def onMessage(self, message_object: fbchat.Message, thread_id: str, thread_type: fbchat.ThreadType, **kwargs):
         if message_object.author == self.uid:
@@ -58,30 +41,19 @@ class Robotczyk(fbchat.Client):
         if not self.is_mentioned(message_object):
             return
 
-        content = self.cleanify(message_object.text)
-        keyword = self.keyword(content)
+        content = self.clean_message(message_object.text)
+        response = self.chain.generate(content)
 
-        dataset = [text for text in self.dataset if keyword in text.lower()]
-
-        chain = markovify.NewlineText(
-            dataset, 
-            state_size=3,
-            well_formed=False
-        )
-
-        text = chain.make_sentence(tries=100000) or "..."
-        message = fbchat.Message(text=text, reply_to_id=message_object.uid)
+        message = fbchat.Message(text=response, reply_to_id=message_object.uid)
 
         self.send(message, thread_id=thread_id, thread_type=thread_type)
     
 if __name__ == "__main__":
     with open("credentials.json", "r", encoding="utf-8") as file:
         credentials = json.load(file)
-
-    with open("dataset.json", "r", encoding="utf-8") as file:
-        dataset = json.load(file)
     
-    client = Robotczyk(dataset, ["@julia robotczyk", "@kornelia krawiec", "@kornelia"], **credentials)
+    chain = ConversationalChain(dataset_path="dataset.json")
+    client = RobotczykClient(chain, **credentials)
     
     try:
         client.listen()
